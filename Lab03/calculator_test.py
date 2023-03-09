d6304d68307d0726638ad3a160cd255fa7a42dff
JianHongWu <pet9687123@gmail.com>
2023-03-09 23:25:25 +0800

    Finish

--- Lab03/calculator_test.py
+++ Lab03/calculator_test.py
@@ -6,7 +6,6 @@ from calculator import Calculator
 class ApplicationTest(unittest.TestCase):
     calc = Calculator()
 
-
     def test_add(self):
         param_list = [(48, 29), (63, 25), (12, 19), (35, 97), (100, 123)]
         for i, j in param_list:
@@ -14,7 +13,6 @@ class ApplicationTest(unittest.TestCase):
             self.assertEqual(n, i + j)
         self.assertRaises(TypeError, self.calc.add, 99, "121")
         pass
-
 
     def test_divide(self):
         param_list = [(48, 29), (63, 25), (12, 19), (35, 97), (100, 123)]
@@ -24,22 +22,20 @@ class ApplicationTest(unittest.TestCase):
         self.assertRaises(ZeroDivisionError, self.calc.divide, 11, 0)
         pass
 
-
     def test_sqrt(self):
         param_list = [12, 45, 96, 87, 25]
         for i in param_list:
             n = self.calc.sqrt(i)
             self.assertEqual(n, math.sqrt(i))
-        self.assertRaises(ValueError, self.calc.sqrt , - 1)
+        self.assertRaises(ValueError, self.calc.sqrt, -1)
         pass
-
 
     def test_exp(self):
         param_list = [12, 45, 96, 87, 25]
         for i in param_list:
             n = self.calc.exp(i)
             self.assertEqual(n, math.exp(i))
-        self.assertRaises(TypeError, self.calc.exp, "1")
+        self.assertRaises(OverflowError, self.calc.exp, 1000000)
         pass
 
 
