6bff347f2761289b29f9e42c8fa632f7b26165ba
JianHongWu <pet9687123@gmail.com>
2023-03-17 00:59:39 +0800

    Finish

--- Lab04/app.py
+++ Lab04/app.py
@@ -32,7 +32,7 @@ driver.get("https://www.google.com")
 google = driver.find_element(By.NAME, 'q')
 google.send_keys("311551126")
 google.submit()
-google = driver.find_elements(By.CLASS_NAME, 'MBeuO')[1]
+google = driver.find_elements(By.CLASS_NAME, 'LC20lb')[1]
 print(google.text)
 driver.close()
 
